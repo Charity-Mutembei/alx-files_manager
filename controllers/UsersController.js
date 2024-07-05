a665745e6e5d09ad6eb18c76bbf3bb8f08d67f2e
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-06 02:57:07 +0300

    task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,32 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+class UsersController {
+    static async postNew(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email' });
+        }
+
+        if (!password) {
+            return res.status(400).json({ error: 'Missing password' });
+        }
+
+        const userCollection = dbClient.db.collection('users');
+        const existingUser = await userCollection.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ error: 'Already exist' });
+        }
+
+        const hashedPassword = sha1(password);
+        const newUser = { email, password: hashedPassword };
+
+        const result = await userCollection.insertOne(newUser);
+
+        return res.status(201).json({ id: result.insertedId, email: newUser.email });
+    }
+}
+
+module.exports = UsersController;
