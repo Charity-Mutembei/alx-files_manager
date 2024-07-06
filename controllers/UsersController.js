dec680683952edd135c56ad7577342cb358b6690
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-06 03:08:53 +0300

    task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
     static async postNew(req, res) {
@@ -26,6 +27,28 @@ class UsersController {
         const result = await userCollection.insertOne(newUser);
 
         return res.status(201).json({ id: result.insertedId, email: newUser.email });
+    }
+
+    static async getMe(req, res) {
+        const token = req.headers['x-token'];
+
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const userId = await redisClient.get(`auth_${token}`);
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const userCollection = dbClient.db.collection('users');
+        const user = await userCollection.findOne({ _id: dbClient.ObjectId(userId) }, { projection: { email: 1 } });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        return res.status(200).json({ id: user._id, email: user.email });
     }
 }
 
